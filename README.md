e113f9ed44c9a36eedce0bc533480e6e2d05d1ff
YA <YA-androidapp@users.noreply.github.com>
2020-07-30 20:36:09 +0900

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Docker-MySQL8
+
+---
+
+## How to use
+
+```ps
+$ docker-compose up -d
+
+$ docker exec -it mysql01 bash
+$ # mysql -u mysqluser -D mysqldb -pmysqlpw
+```
---
+++ init/010-createdb.sql
@@ -0,0 +1,10 @@
+﻿-- DROP DATABASE IF EXISTS mysqldb;
+
+CREATE DATABASE IF NOT EXISTS mysqldb;
+
+USE mysqldb;
+
+-- DROP TABLE IF EXISTS mysqltable;
+
+-- CREATE TABLE mysqltable
+-- INSERT INTO mysqltable
